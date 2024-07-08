6f9c117dfffaed05ddc2e42d1c4a053ba7ca2110
Johnson Omwoyo <johnsonomwoyo100@gmail.com>
2024-07-08 22:27:20 +0300

    DOne writing a fetch

---
+++ index.js
@@ -0,0 +1,18 @@
+const init = () => {
+  const inputform = document.querySelector("form");
+  const input = document.querySelector("input#searchByID");
+  inputform.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    fetch("http://localhost:3000/movies" + input.value)
+      .then((response) => response.json())
+      .then((data) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+
+    title.innerText = data.title;
+    summary.innerText = data.summary;
+      });
+  });
+};
+document.addEventListener("DOMContentLoaded", init);
